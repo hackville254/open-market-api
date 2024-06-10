b125af78f22201c6124eb0608f5672f986c2e402
hackville254 <fdelbo47@gmail.com>
2024-06-10 21:33:43 +0200

    modification admin  debug=false

--- authentification/admin.py
+++ authentification/admin.py
@@ -5,9 +5,9 @@ from .models import Entreprise,Notification
 class EntrepriseAdmin(admin.ModelAdmin):
     list_display = ('slug', 'nom_entreprise', 'pays', 'ville', 'numero', 'date_modification', 'date')
     list_display_links = ('slug', 'nom_entreprise')  # Les champs cliquables pour accéder à la modification
-
-
-
+    search_fields = ('nom_entreprise', 'pays', 'ville', 'numero', 'email', 'secteur_activite')
+    list_filter = ('pays','date')
+    ordering = ('-date',)  # Afficher les plus récents en premier
 
 admin.site.register(Entreprise , EntrepriseAdmin)
 
--- produits/admin.py
+++ produits/admin.py
@@ -7,17 +7,26 @@ from django.contrib import admin
 class ProduitNumeriqueAdmin(admin.ModelAdmin):
     list_display = ('slug', 'nom_produit', 'gratuit', 'prix_produit', 'langue_produit', 'categorie_produit','supprime', 'date_modification', 'date')
     list_display_links = ('slug', 'nom_produit')  # Les champs cliquables pour accéder à la modification
+    ordering = ('-date',)  # Afficher les plus récents en premier
 
 class LivreAdmin(admin.ModelAdmin):
     list_display = ('slug', 'nom_produit', 'gratuit', 'prix_produit', 'langue_produit', 'categorie_produit','supprime', 'date_modification', 'date')
     list_display_links = ('slug', 'nom_produit')  # Les champs cliquables pour accéder à la modification
+    ordering = ('-date',)  # Afficher les plus récents en premier
 
 class AcceAdmin(admin.ModelAdmin):
     list_display = ('slug', 'nom_produit', 'gratuit', 'prix_produit', 'langue_produit', 'categorie_produit','supprime', 'date_modification', 'date')
     list_display_links = ('slug', 'nom_produit')  # Les champs cliquables pour accéder à la modification
+    ordering = ('-date',)  # Afficher les plus récents en premier
+
 
 class CHECKOUTAdmin(admin.ModelAdmin):
-    list_display = ('slug','produit','date_modification', 'date')
+    list_display = ('slug', 'reference', 'produit', 'entreprise', 'nom_client', 'status', 'type', 'pays_client', 'moyen_de_paiement', 'numero', 'email', 'date_modification', 'date')
+    list_display_links = ('slug', 'reference')  # Les champs cliquables pour accéder à la modification
+    search_fields = ('reference', 'nom_client', 'status', 'type', 'pays_client', 'moyen_de_paiement', 'numero', 'email')
+    list_filter = ('status', 'type', 'pays_client', 'moyen_de_paiement', 'date')
+    ordering = ('-date',)  # Afficher les plus récents en premier
+
 
 class FichierAdmin(admin.ModelAdmin):
     list_display = ('produit',)
